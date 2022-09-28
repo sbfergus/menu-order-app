bb44e9baa8b92e0f7f15aaaba8b7971dd9bcda9c
Scott <sbferguson17@gmail.com>
2022-09-28 17:26:31 -0600

    Header component and styles

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table full of food" />
+      </div>
+    </>
+  );
+};
