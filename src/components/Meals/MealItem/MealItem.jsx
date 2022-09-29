d166f753f23991e3ba651a57e41ebe81476ada2f
Scott <sbferguson17@gmail.com>
2022-09-28 18:29:48 -0600

    Input form component and styles

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -11,7 +11,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
